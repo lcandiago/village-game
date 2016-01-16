d81a7ac7a71fcbd53032b10ff4bbe9b06931b87b
Lorenzo Candiago <lorenzo@organicadigital.com>
2016-01-16 20:31:45 -0200

    Improve JS

--- js/main.js
+++ js/main.js
@@ -1,43 +1,42 @@
-// Global Variables
-var game = false
-var wolf = 0;
-var people = 0;
-var roles = [];
+settings();
 
 // Game Settings
 function settings(){
-  // Number of players
-  wolf = document.getElementById("wolf").val;
-  people = document.getElementById("people").val;
-
-  for (i = wolf; i > 0; i--) {
-    roles.push("Lobo");
-  }
-
-  for (i = people; i > 0; i--) {
-    if (i == 2) {
-      roles.push("Cartomante");
-    }
-    else if (i == 1) {
-      roles.push("Feiticeira");
-    }
-    else {
-      roles.push("Vileiro");
-    }
-  }
+  var game = false
+  var wolf = 0;
+  var people = 0;
+  var roles = [];
 
   var element = document.getElementById("play");
-  element.addEventListener("click", function() { game() }, false);
+  element.addEventListener("click", function() { startGame(game, wolf, people, roles) }, false);
 }
 
-function game(){
-  if (game == false) {
-    players = document.getElementById("num");
-    if (players < 4) {
+function startGame(game, wolf, people, roles){
+  if (game == false){
+    // Number of players
+    wolf = $("#wolf").val();
+    people = $("#people").val();
 
+    for (i = wolf; i > 0; i--) {
+      roles.push("Lobo");
     }
-    else if (players <= )
+
+    for (i = people; i > 0; i--) {
+      if (i == 2) {
+        roles.push("Cartomante");
+      }
+      else if (i == 1) {
+        roles.push("Feiticeira");
+      }
+      else {
+        roles.push("Vileiro");
+      }
+    }
+
+    game = true;
+    alert(roles);
   }
+
   var nights = document.getElementById("nights");
 
   if (nights.val == "0"){
